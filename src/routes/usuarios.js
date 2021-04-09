d023e7e17c58bdc9052bc754eec8d7112a540579
Lucas  Souza Annunziato <lucassouzaannunziato@gmail.com>
2021-04-08 21:46:07 -0300

    Update no tempo do Rate Limiter

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -5,9 +5,9 @@ const userController = require('../controllers/usuarios');
 const router = Router();
 //WindowMs define o tempo de requisição e o max é o número máximo de requisições, não é o melhor jeito, mas para fins de testes vamos fazer desta forma --06.04.2021
 const apiLimiter = rateLimit({
-    windowMs: 24 * 60 * 60 * 1000, // 24 hrs
-    max: 2,
-    message: 'O limite de requisição foi excedido, mais de 100 em 1 dia.', 
+    windowMs: 2 * 60 * 1000, // 2 min
+    max: 150,
+    message: 'O limite de requisição foi excedido, tente novamente mais tarde.', 
     headers: true,
 });
 
